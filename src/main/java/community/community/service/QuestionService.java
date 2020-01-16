eb5c50cd524e56775e67d1fe1ed0a2cad9c2b748
“test668 <2208700170@qq.com>
2020-01-16 20:11:33 +0800

    question description

--- src/main/java/community/community/service/QuestionService.java
+++ src/main/java/community/community/service/QuestionService.java
@@ -96,4 +96,13 @@ public class QuestionService {
 
         return paginationDto;
     }
+
+    public QuestionDto getById(Integer id) {
+        Question question= questionMapper.getById(id);
+        QuestionDto questionDto=new QuestionDto();
+        BeanUtils.copyProperties(question,questionDto);
+        User user=userMapper.findById(question.getCreator());
+        questionDto.setUser(user);
+        return questionDto;
+    }
 }
